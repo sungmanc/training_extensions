55b79cccd5da7d2ccb7d93706e5e2844675c99d0
Jihwan Eom <jihwan.eom@intel.com>
2022-05-27 01:51:23 +0900

    increse batch_size to 4

--- external/mmsegmentation/tests/test_ote_training.py
+++ external/mmsegmentation/tests/test_ote_training.py
@@ -122,6 +122,12 @@ class SegmentationTrainingTestParameters(DefaultOTETestCreationParametersInterfa
         ]
         return deepcopy(test_bunches)
 
+    def default_test_parameters(self) -> Dict[str, Any]:
+        DEFAULT_TEST_PARAMETERS = {
+            "batch_size": 4,
+        }
+        return deepcopy(DEFAULT_TEST_PARAMETERS)
+
 def get_dummy_compressed_model(task):
     """
     Return compressed model without initialization
@@ -276,8 +282,5 @@ class TestOTEReallifeSegmentation(OTETrainingTestInterface):
              test_parameters,
              test_case_fx, data_collector_fx,
              cur_test_expected_metrics_callback_fx):
-        if "18_OCR" in test_parameters["model_name"] \
-                or "x-mod3_OCR" in test_parameters["model_name"]:
-            pytest.skip("Known issue CVS-83781")
         test_case_fx.run_stage(test_parameters['test_stage'], data_collector_fx,
-                               cur_test_expected_metrics_callback_fx)
+                            cur_test_expected_metrics_callback_fx)

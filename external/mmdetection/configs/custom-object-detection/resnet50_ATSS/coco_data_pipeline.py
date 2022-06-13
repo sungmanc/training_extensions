05141c70735979748f4dabff59e389bf7ae55ccd
Cho, Sungman <sungman.cho@intel.com>
2022-06-13 13:40:14 +0900

    update R50 ATSS configuration

--- external/mmdetection/configs/custom-object-detection/resnet50_ATSS/coco_data_pipeline.py
+++ external/mmdetection/configs/custom-object-detection/resnet50_ATSS/coco_data_pipeline.py
@@ -1,36 +1,30 @@
 dataset_type = 'CocoDataset'
-img_size = (992, 736)
+img_size = (1333, 800)
 img_norm_cfg = dict(mean=[0, 0, 0], std=[255, 255, 255], to_rgb=True)
 
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(
-        type='MinIoURandomCrop',
-        min_ious=(0.1, 0.3, 0.5, 0.7, 0.9),
-        min_crop_size=0.3),
-    dict(
-        type='Resize',
-        img_scale=[(992, 736), (896, 736), (1088, 736), (992, 672), (992, 800)],
-        multiscale_mode='value',
-        keep_ratio=False),
+    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
 ]
 test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=img_size,
+        img_scale=(1333, 800),
         flip=False,
         transforms=[
-            dict(type='Resize', keep_ratio=False),
+            dict(type='Resize', keep_ratio=True),
             dict(type='RandomFlip'),
             dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
             dict(type='ImageToTensor', keys=['img']),
-            dict(type='Collect', keys=['img'])
+            dict(type='Collect', keys=['img']),
         ])
 ]
 data = dict(

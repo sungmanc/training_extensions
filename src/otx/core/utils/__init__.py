6228ba8954341b68e143709761e08b4edbe071b7
Kim, Vinnam <vinnam.kim@intel.com>
2023-11-24 14:07:11 +0900

    Update license
    Signed-off-by: Kim, Vinnam <vinnam.kim@intel.com>

--- src/otx/core/utils/__init__.py
+++ src/otx/core/utils/__init__.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 # Copyright (C) 2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 #
-=======
->>>>>>> 801ddae17 (Initial commit)
 """Module for utility functions in OTX."""
